e5d1dc6dd9fbc8f1c35b3a30aaaa3bc727c9117f
Sametakkaya <burak_samet19@hotmail.com>
2022-11-19 16:18:21 +0300

    Kart Projesi Kodları

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kart';
+  posts = [
+    {
+      title: 'Dağ Bisikleti',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'cbbik',
+      content: 'Çok Hızlı Sürdüm',
+    },
+    {
+      title: 'Tırmanış',
+      imageUrl: 'assets/mountain.jpg',
+      username: 'cbdag',
+      content: 'Hadi Tırmanalım',
+    },
+    {
+      title: 'Doğa Yürüyüşü',
+      imageUrl: 'assets/tree.jpg',
+      username: 'cbtırman',
+      content: 'Hadi Biraz Turlayalım',
+    },
+  ];
 }
